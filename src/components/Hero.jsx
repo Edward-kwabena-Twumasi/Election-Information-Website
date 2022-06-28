fea38c88a42f317630b3b127832fdd5974f7d378
Edward <createdliving1000@gmail.com>
2022-06-28 21:16:05 +0000

    React tailwind app

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+const Hero=()=> {
+    return (  <>
+    <div className='text-white'>
+        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+    <p className='text-[#00df9a] font-bold p-2'> GROWING WITH TECHNOLOGY</p>
+    <h1 className='md:text-6xl sm:text-5xl text-3xl font-bold md:py-6'>Grow with our innovative Technology</h1>
+    <div>
+        <p className='md:text-4xl sm:text-3xl text-xl font-bold'>Intuitive, smart, interactive ai software for</p>
+    </div>
+        </div>
+    </div>
+    </>);
+}
+
+export default Hero;
